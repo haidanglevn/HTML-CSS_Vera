8df68351c47fe3b28572e89d3dd9ab3276ceb24b
haidanglevn <haidang.levn@gmail.com>
2022-10-06 14:50:58 +0300

    update project page for portfolio

--- Portfolio (Last Ver)/script.js
+++ Portfolio (Last Ver)/script.js
@@ -1,36 +1,22 @@
-let leaderboard = document.querySelector(".leaderboard");
-let order = 0;
-let playerName;
-let score, i;
-let scoreList = [];
-class Score {
-  constructor(playerName, point) {
-    this.playerName = playerName;
-    this.point = point;
-  }
-}
+projectImg = document.getElementsByClassName("project-img");
+modal = document.querySelectorAll(".modal");
+modalContainer = document.querySelector(".modal-content");
+let link, title, p, modalContent;
+console.log("modal ", modal);
 
-const addScore = () => {
-  playerName = document.getElementById("playerName").value;
-  point = document.getElementById("point").value;
-  let newScore = new Score(playerName, point);
-  scoreList.unshift(newScore);
-  console.table(scoreList);
-  console.log(scoreList[0].playerName);
-  const updateScore = () => {
-    let scoreBoard = document.querySelector(".leaderboard-content");
-    let list;
-    if (scoreList.length <= 5) {
-      list = `<div class="attempt">${scoreList[0].playerName}: ${scoreList[0].point} </div>`;
-      scoreBoard.innerHTML += list;
-    } else {
-      list = `<div class="attempt">${scoreList[4].playerName}: ${scoreList[4].point} </div>
-        <div class="attempt">${scoreList[3].playerName}: ${scoreList[3].point} </div>
-        <div class="attempt">${scoreList[2].playerName}: ${scoreList[2].point} </div>
-        <div class="attempt">${scoreList[1].playerName}: ${scoreList[1].point} </div>
-        <div class="attempt">${scoreList[0].playerName}: ${scoreList[0].point} </div>`;
-      scoreBoard.innerHTML = list;
+const openModal = (value) => {
+  console.log(value);
+  modal.forEach((content) => {
+    if (content.id == value) {
+      console.log("content.id: ", content.id);
+      document.getElementById(content.id).style.display = "block";
+      document.body.style.overflow = "hidden";
     }
-  };
-  updateScore();
+  });
+};
+const closeModal = () => {
+  modal.forEach((content) => {
+    content.style.display = "none";
+    document.body.style.overflow = "auto";
+  });
 };
